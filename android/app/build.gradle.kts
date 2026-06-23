3d448ae3a2e1e2717b2412da9ed28a32aca5b2a7
4evergr8 <137018415+4evergr8@users.noreply.github.com>
2026-06-23 11:36:13 +0800

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "a.forevergreat.rss"
+    namespace = "app.flutter.rssqlite"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "a.forevergreat.rss"
+        applicationId = "app.flutter.rssqlite"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
