b746f3096fd8e123ef0b52218431bfc694a58567
4evergr8 <137018415+4evergr8@users.noreply.github.com>
2026-06-23 11:38:06 +0800

    Update settings.dart

--- lib/view/settings.dart
+++ lib/view/settings.dart
@@ -5,8 +5,8 @@ import 'package:drift/drift.dart' as drift;
 import 'package:flutter/material.dart';
 import 'package:path/path.dart' as p;
 import 'package:path_provider/path_provider.dart';
-import 'package:rss/database.dart';
-import 'package:rss/widget.dart';
+import 'package:rssqlite/database.dart';
+import 'package:rssqlite/widget.dart';
 import 'package:share_plus/share_plus.dart';
 
 final _db = AppDatabase();
